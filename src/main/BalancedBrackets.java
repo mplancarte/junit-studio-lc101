c1b3f382615d9834b8c3ca54be92cbb160b147f0
miguel plancarte <plancarte2@gmail.com>
2020-05-07 18:56:58 -0500

    Finished 12 tests for real this time

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -5,18 +5,18 @@ public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
